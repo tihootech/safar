f9f2f8dd86c9fbd034e9fdb985691f799712a7c4
seyfi68 <ali.seyfi68@gmail.com>
2020-10-10 15:24:20 +0330

    Admin Seeder

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'admin',
+            'email' => 'admin',
+            'email_verified_at' => now(),
+            'password' => bcrypt('admin'),
+            'type' => 'admin',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+    }
+}
