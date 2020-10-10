b345a447d83f6f4e45a2593d29f45c8d137b94b8
seyfi68 <ali.seyfi68@gmail.com>
2020-10-10 15:36:25 +0330

    laravel Synatra Template uploaded

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,48 +1,96 @@
-<x-guest-layout>
-    <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+        <meta name="description" content="">
+        <meta name="author" content="Mannat Themes">
+        <meta name="keyword" content="">
 
-        <x-jet-validation-errors class="mb-4" />
+        <title>SYNTRA | Responsive Web App Kit</title>
 
-        @if (session('status'))
-            <div class="mb-4 font-medium text-sm text-green-600">
-                {{ session('status') }}
+        <!-- Theme icon -->
+        <link rel="shortcut icon" href="{{asset('assets/images/favicon.ico')}}">
+
+        <!-- Theme Css -->
+        <link href="{{asset('assets/css/bootstrap.min.css')}}" rel="stylesheet">
+        <link href="{{asset('assets/css/slidebars.min.css')}}" rel="stylesheet">
+        <link href="{{asset('assets/css/icons.css')}}" rel="stylesheet">
+        <link href="{{asset('assets/css/menu.css')}}" rel="stylesheet" type="text/css">
+        <link href="{{asset('assets/css/style.css')}}" rel="stylesheet">
+    </head>
+
+    <body class="sticky-header">
+        <section class="bg-login">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 col-sm-12">
+                        <div class="wrapper-page">
+                            <div class="account-pages">
+                                <div class="account-box">
+                                    <div class="card m-b-30">
+                                        <div class="card-body">
+                                            <div class="card-title text-center">
+                                                <img src="assets/images/logo_sm_2.png" alt="" class="">
+                                                <h5 class="mt-3"><b>Welcome to Syntra</b></h5>
+                                            </div>
+                                            <form class="form mt-5 contact-form" action="index.html">
+                                                <div class="form-group ">
+                                                    <div class="col-sm-12">
+                                                        <input class="form-control form-control-line" type="text" placeholder="Username" required="required">
+                                                    </div>
+                                                </div>
+                                                <div class="form-group ">
+                                                    <div class="col-sm-12">
+                                                        <input class="form-control form-control-line" type="password" placeholder="Password" required="required">
+                                                    </div>
+                                                </div>
+
+                                                <div class="form-group">
+                                                    <div class="col-12">
+                                                        <label class="cr-styled">
+                                                            <input type="checkbox" checked>
+                                                            <i class="fa"></i>
+                                                            Remember me
+                                                        </label>
+                                                    </div>
+                                                </div>
+
+                                                <div class="form-group">
+                                                    <div class="col-sm-12 mt-4">
+                                                        <button class="btn btn-primary btn-block" type="submit">Log In</button>
+                                                    </div>
+                                                </div>
+
+                                                <div class="form-group">
+                                                    <div class="col-sm-12 mt-4 text-center">
+                                                        <a href="recoverpw.html"><i class="fa fa-lock m-r-5"></i> Forgot password?</a>
+                                                    </div>
+                                                </div>
+
+
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-        @endif
+        </section>
 
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
+        <!-- jQuery -->
+        <script src="{{asset('assets/js/jquery-3.2.1.min.js')}}"></script>
+        <script src="{{asset('assets/js/popper.min.js')}}"></script>
+        <script src="{{asset('assets/js/bootstrap.min.js')}}"></script>
+        <script src="{{asset('assets/js/jquery-migrate.js')}}"></script>
+        <script src="{{asset('assets/js/modernizr.min.js')}}"></script>
+        <script src="{{asset('assets/js/jquery.slimscroll.min.js')}}"></script>
+        <script src="{{asset('assets/js/slidebars.min.js')}}"></script>
 
-            <div>
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            </div>
 
-            <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
-            </div>
-
-            <div class="block mt-4">
-                <label for="remember_me" class="flex items-center">
-                    <input id="remember_me" type="checkbox" class="form-checkbox" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-                <x-jet-button class="ml-4">
-                    {{ __('Login') }}
-                </x-jet-button>
-            </div>
-        </form>
-    </x-jet-authentication-card>
-</x-guest-layout>
+        <!--app js-->
+        <script src="{{asset('assets/js/jquery.app.js')}}"></script>
+    </body>
+</html>
